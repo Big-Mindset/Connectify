8fe75689d0ac293f6f6c0f2d11257924ff0ad4a8
Big-Mindset <wadoodmemon0@gmail.com>
2025-07-26 14:32:31 -0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import {auth} from "@/lib/auth"
+import { auth } from "./lib/auth";
 
 // Define route constants
 const PUBLIC_ROUTES = ["/sign-up", "/sign-in"];
