a6332cfbeb374eea0161c9e99b8a877e7d921bdb
Big-Mindset <wadoodmemon0@gmail.com>
2025-07-26 07:54:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -284,7 +284,7 @@ app.prepare().then(() => {
         });
     });
 
-    server.listen(port || '0.0.0.0', () => {
+    server.listen(port , '0.0.0.0', () => {
         console.log("> Server running on http://localhost:3000");
     });
 });
