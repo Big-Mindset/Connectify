6c3332c6a6f0eb20c452479653039b2f4b078c9f
Big-Mindset <wadoodmemon0@gmail.com>
2025-09-23 00:46:12 +0500

    done

--- src/app/(userAuth)/api/updateProfile/route.js
+++ src/app/(userAuth)/api/updateProfile/route.js
@@ -5,14 +5,12 @@ import { NextResponse } from "next/server"
 export async function PUT(req) {
         try {
             let data = await req.json()
-            console.log(data);
             
                         if (data.avatar){
                 let {secure_url} = await cloudinary.uploader.upload(data.avatar)
                 data.avatar = secure_url 
             }
             let {id , ...rest} = data
-            console.log(id , rest);
             
             await prisma.account.update({
                 where : {
@@ -24,9 +22,8 @@ export async function PUT(req) {
             })
             return NextResponse.json({Message : "Updated"},{status : 200})
         } catch (error) {
-            console.log(error.message);
             
-            return NextResponse.json({Message : "Internal Server Error"},{status : 500})
+            return NextResponse.json({Message : "Internal Server Error",error },{status : 500})
             
         }
 }
