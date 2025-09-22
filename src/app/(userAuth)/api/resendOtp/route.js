ff0e5ff4e8157b3ce8bfe030d9a161b8b4c8671c
Big-Mindset <wadoodmemon0@gmail.com>
2025-09-23 00:28:55 +0500

    done

--- src/app/(userAuth)/api/resendOtp/route.js
+++ src/app/(userAuth)/api/resendOtp/route.js
@@ -1,33 +1,36 @@
 
 import { EmailVerification } from "@/components/EmailTemplate";
-import { transporter } from "@/lib/nodemailer";
 import { client } from "@/lib/redis";
 import { NextResponse } from "next/server"
+const resend = new Resend(process.env.RESEND_API_KEY);
                                                                                                                
 export async function PUT(req) {
     try {
         let {token,email,name} = await req.json()
-        console.log(email , name);
         
         if (!token){
             return NextResponse.json({Message : "Something went wrong try again"})
         }
         let otp = Date.now().toString().slice(7).toUpperCase()
         let expiry =( Date.now() + 50 *1000)
-        await transporter.sendMail({
-            from: process.env.GMAIL_USER,
-            to: email,
-            subject: "Test Email",
-            html: EmailVerification( name, email , otp),
-          });
           
+       const {  data : emailData , error } = await resend.emails.send({
+            from: 'Connectify <onboarding@resend.dev>', // Use resend.dev for testing
+            to: [data.email],
+            subject: 'Connectify Verification Email',
+            html: EmailVerification(name, email, otp),
+        });
+        if (error){
+            console.log(error);
+        }
+        console.log(emailData);
+        
         await client.hSet(`User_Session-${token}`,{
             otp : otp,
             otpExpiresAt : expiry.toString()
         })
         return NextResponse.json({Message : "Rsend Successfully"},{status : 201})
     }catch (err){
-        console.log(err.message);
         
         return NextResponse.json({Message : "Internal Server Error"},{status : 500})
     }
